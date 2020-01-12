180e41fdc991b88d1897e5cb888440ed95d9fc99
Davi <Davidmaccurso@gmail.com>
2020-01-12 19:17:04 -0300

    creada la interfaz de album

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map'
 import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 @Injectable()
 export class ProductService {
@@ -10,8 +11,8 @@ export class ProductService {
 
   constructor(private _http: Http) { }
 
-  public getAlbum(id: number) {
+  public getAlbum(id: number): Observable<Album> {
     return this._http.get(this._albumUrl).map((response) =>
-      response.json());
+      <Album>response.json());
   }
 }
---
+++ src/app/track.ts
@@ -0,0 +1,6 @@
+export interface Track {
+    trackNumber: number;
+    tackName: string;
+    trackLenght: string;
+    trackPrice: number;
+}
